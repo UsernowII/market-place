49ece2afa7f9509ab1cbbeb9f3dd058cbe813afb
usernow <usernow.82@gmail.com>
2025-04-10 12:42:04 -0500

    stats service application

--- stats-service/src/main/java/com/market/statsservice/event/KafkaConsumerConfig.java
+++ stats-service/src/main/java/com/market/statsservice/event/KafkaConsumerConfig.java
@@ -3,6 +3,7 @@ package com.market.statsservice.event;
 import com.market.couponservice.model.dto.ItemsEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -17,10 +18,13 @@ import java.util.Map;
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
     @Bean
     public ConsumerFactory<String, ItemsEvent> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "stats-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
