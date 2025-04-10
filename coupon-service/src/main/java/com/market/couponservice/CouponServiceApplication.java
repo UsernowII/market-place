fa06ecfcfdb17c71dd8b304c7bb704f55584635a
usernow <usernow.82@gmail.com>
2025-04-10 12:38:24 -0500

    coupon application

--- coupon-service/src/main/java/com/market/couponservice/CouponServiceApplication.java
+++ coupon-service/src/main/java/com/market/couponservice/CouponServiceApplication.java
@@ -2,12 +2,10 @@ package com.market.couponservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableFeignClients
 public class CouponServiceApplication {
 
     public static void main(String[] args) {
--- coupon-service/src/main/java/com/market/couponservice/config/KafkaProducerConfig.java
+++ coupon-service/src/main/java/com/market/couponservice/config/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package com.market.couponservice.config;
 import com.market.couponservice.model.dto.ItemsEvent;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -16,10 +17,13 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ProducerFactory<String, ItemsEvent> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
@@ -27,7 +31,7 @@ public class KafkaProducerConfig {
     }
 
     @Bean
-    public KafkaTemplate<String, ItemsEvent> ItemKafkaTemplate() {
+    public KafkaTemplate<String, ItemsEvent> itemKafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
